87bef40537e4ca5f2d2854f19f6cf0c92af36189
lanbery <cwd-pc@xy-info.com>
2024-12-22 21:59:40 +0800

    test: add test dependencies

--- tests/src/cache-redis/redis.client.controller.ts
+++ tests/src/cache-redis/redis.client.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, HttpCode, HttpStatus, Get } from '@nestjs/common';
+import { Controller, Post, HttpCode, HttpStatus, Get,Query,Body } from '@nestjs/common';
 import { RedisCliTestService } from './redis.client.service';
 
 @Controller('e2e/rediscli')
@@ -6,15 +6,18 @@ export class RedisClientController {
   constructor(private readonly clientService: RedisCliTestService) {}
 
   @Post('set')
-  // @HttpCode(HttpStatus.OK)
+  @HttpCode(HttpStatus.OK)
   async set(@Body() body: { key: string; value: string }): Promise<any> {
     const { key, value } = body;
     return this.clientService.setCache(key, value);
   }
 
   @Get('get/spec_key')
-  // @HttpCode(HttpStatus.OK)
-  async get(@Query() querys: { key: string }) {
+  @HttpCode(HttpStatus.OK)
+  async get(
+    @Query() 
+    querys: { key: string }
+  ) {
     const { key } = querys;
     return this.clientService.getCache(key);
   }
